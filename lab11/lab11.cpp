f96ffc190c1becc53cb9e59570d5bcd29eab6cea
Roque Guerrero <rguerrero003@student.butte.edu>
2017-08-25 01:51:59 +0000

    Lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,6 @@
+1.	User will start with game 10 points
+2.	Computer will pick a random number between 1 and 200
+3.	User picks a number
+a.	If user picks correct number, he will win the game and keep his 10 points.
+b.	If user picks the wrong number, the computer will notify him higher or lower and deduct 1 point.
+c.	If user uses all 10 point the game will end and reveal the chosen number.
