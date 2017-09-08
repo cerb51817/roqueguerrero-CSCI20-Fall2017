6358f2e3e375cd8fa528ae8929a74eef457bc474
Roque Guerrero <rguerrero003@student.butte.edu>
2017-09-08 01:43:53 +0000

    Lab1.5 submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -10,7 +10,11 @@ int main() {
     string nounTwo = "sky";
     string pluralNounOne = "cats";
     string pluralNounTwo = "ducks";
+    string pluralNounThree = "dogs";
     string gameOne = "tic tac toe";
+    string verbEndingINGone = "flying";
+    string verbEndingINGtwo = "running";
+    string verbEndingINGthree = "rainning";
     
     cout << "A vacation is when you take a trip to some " << adjectiveOne << " place with your " << adjectiveTwo << " family.";
     cout << endl;
@@ -18,7 +22,10 @@ int main() {
     cout << endl;
     cout << "A good vacation place is one where you can ride " << pluralNounOne << " or play " << gameOne << " or go hunting for " << pluralNounTwo << ".";
     cout << endl;
-    
+    cout << "I like to spend my time " << verbEndingINGone << " or " << verbEndingINGtwo << ".";
+    cout << endl;
+    cout << "When parents go on a vacation, they spend their time eating three " << pluralNounThree << " a day, and fathers play golf, and mothers sit around " << verbEndingINGthree << ".";
+    cout << endl;
     
 
     return 0;
