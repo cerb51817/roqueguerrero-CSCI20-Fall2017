f4698a66abfbd6e120234d83290fa3c21981b989
Roque Guerrero <rguerrero003@student.butte.edu>
2017-09-08 03:06:05 +0000

    Lab1.5 submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -4,17 +4,42 @@ using namespace std;
 
 int main() {
      
-    string adjectiveOne = "warm";
-    string adjectiveTwo = "cold";
-    string nounOne = "mountain";
-    string nounTwo = "sky";
-    string pluralNounOne = "cats";
-    string pluralNounTwo = "ducks";
-    string pluralNounThree = "dogs";
-    string gameOne = "tic tac toe";
-    string verbEndingINGone = "flying";
-    string verbEndingINGtwo = "running";
-    string verbEndingINGthree = "rainning";
+    string adjectiveOne;
+    string adjectiveTwo;
+    string nounOne;
+    string nounTwo;
+    string pluralNounOne;
+    string pluralNounTwo;
+    string pluralNounThree;
+    string gameOne;
+    string verbEndingINGone;
+    string verbEndingINGtwo;
+    string verbEndingINGthree;
+    
+    cout << "Enter adjective one? ";
+    cin >> adjectiveOne;
+    cout << "Enter adjective two? ";
+    cin >> adjectiveTwo;
+    cout << "Enter noun one? ";
+    cin >> nounOne;
+    cout << "Enter noun two? ";
+    cin >> nounTwo;
+    cout << "Enter plural noun one? ";
+    cin >> pluralNounOne;
+    cout << "Enter plural noun two? ";
+    cin >> pluralNounTwo;
+    cout << "Enter plural noun three? ";
+    cin >> pluralNounThree;
+    cout << "Enter game one? ";
+    cin >> gameOne;
+    cout << "Enter verb ending in ING one? ";
+    cin >> verbEndingINGone;
+    cout << "Enter verb ending in ING two? ";
+    cin >> verbEndingINGtwo;
+    cout << "Enter verb ending in ING three? ";
+    cin >> verbEndingINGthree;
+    
+    
     
     cout << "A vacation is when you take a trip to some " << adjectiveOne << " place with your " << adjectiveTwo << " family.";
     cout << endl;
