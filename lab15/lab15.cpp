7dfae8da820a17ff4245d56dcd19c7d421026035
Roque Guerrero <rguerrero003@student.butte.edu>
2017-09-08 01:24:39 +0000

    Lab1.5 submission

--- lab15/lab15.cpp
+++ lab15/lab15.cpp
@@ -13,8 +13,10 @@ int main() {
     string gameOne = "tic tac toe";
     
     cout << "A vacation is when you take a trip to some " << adjectiveOne << " place with your " << adjectiveTwo << " family.";
-    cout << " Usually you go to some place that is near a " << nounOne << " or up on a " << nounTwo << ".";
-    cout << " A good vacation place is one where you can ride " << pluralNounOne << " or play " << gameOne << " or go hunting for " << pluralNounTwo << " .";
+    cout << endl;
+    cout << "Usually you go to some place that is near a " << nounOne << " or up on a " << nounTwo << ".";
+    cout << endl;
+    cout << "A good vacation place is one where you can ride " << pluralNounOne << " or play " << gameOne << " or go hunting for " << pluralNounTwo << ".";
     cout << endl;
     
     
