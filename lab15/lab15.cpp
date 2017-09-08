5568f95f7de428783346b04754b789e5e1de0999
Roque Guerrero <rguerrero003@student.butte.edu>
2017-09-08 01:21:57 +0000

    Lab1.5 submission

---
+++ lab15/lab15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+     
+    string adjectiveOne = "warm";
+    string adjectiveTwo = "cold";
+    string nounOne = "mountain";
+    string nounTwo = "sky";
+    string pluralNounOne = "cats";
+    string pluralNounTwo = "ducks";
+    string gameOne = "tic tac toe";
+    
+    cout << "A vacation is when you take a trip to some " << adjectiveOne << " place with your " << adjectiveTwo << " family.";
+    cout << " Usually you go to some place that is near a " << nounOne << " or up on a " << nounTwo << ".";
+    cout << " A good vacation place is one where you can ride " << pluralNounOne << " or play " << gameOne << " or go hunting for " << pluralNounTwo << " .";
+    cout << endl;
+    
+    
+
+    return 0;
+}
